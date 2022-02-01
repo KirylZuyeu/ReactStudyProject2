46c558d91e6670d2ddb80d2e6a0c011d79edc11d
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2022-02-01 20:22:07 +0300

    feat: add propTypes librarry that help us to validate type of input variables

--- react-youtube/src/Todo/TodoItem.js
+++ react-youtube/src/Todo/TodoItem.js
@@ -1,7 +1,15 @@
 import React from "react";
+import PropTypes from 'prop-types'
 
-export default function TodoItem({todo}) {
+function TodoItem({todo, index}) {
     return (
-        <li>{todo.title}</li>
+        <li>{index + 1}. {todo.title}</li>
     )
 }
+
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired,
+    index: PropTypes.number
+}
+
+export default TodoItem
--- react-youtube/src/Todo/TodoList.js
+++ react-youtube/src/Todo/TodoList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types'
 import TodoItem from './TodoItem'
 
 const styles = {
@@ -9,12 +10,18 @@ const styles = {
     }
 }
 
-export default function TodoList(props) {
+function TodoList(props) {
     return (
         <ul style={styles.ul}>
-            {props.todos.map(todo => {
-                return <TodoItem todo={todo} key={todo.id}/>
+            {props.todos.map((todo, index) => {
+                return <TodoItem todo={todo} key={todo.id} index={index}/>
             })}
         </ul>
     )
 }
+
+TodoList.propTypes = {
+    todos: PropTypes.arrayOf(PropTypes.object).isRequired,
+}
+
+export default TodoList
