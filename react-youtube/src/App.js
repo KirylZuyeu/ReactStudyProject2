4091037e70a2e7dba7f7356bd3592a53aca022dd
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2022-02-02 00:09:13 +0300

    feat: add input form

--- react-youtube/src/App.js
+++ react-youtube/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import TodoList from './Todo/TodoList'
 import Context from './context'
+import AddTodo from './Todo/AddTodo'
 
 function App() {
   const [todos, setTodos] = React.useState([
@@ -23,11 +24,20 @@ function App() {
     setTodos(todos.filter(todo => todo.id !== id))
   }
 
+  function addTodo(title) {
+    setTodos(todos.concat([{
+      title: title,
+      id: Date.now(),
+      completed: false
+    }]))
+  }
+
   return (
     <Context.Provider value={{removeTodo: removeTodo}}>
       <div className='wrapper'>
-        <h1>Hello world</h1>
-
+        
+        <h1>React Tutorial</h1>
+        <AddTodo onCreate={addTodo}/>
         {todos.length ? (<TodoList todos={todos} onToggle={toggleTodo}/>) : (<p>No todos!</p>)}
 
       </div>
